4437b350c9fc3003a3af6acd7ca13c0ba32f79c2
karelVR <karel.vanroey@cegeka.com>
2020-05-13 12:46:47 +0200

    Process secured-users.json in a slightly different way.
    Problem: java.io.FileNotFoundException: class path resource [secured-users.json] cannot be resolved to absolute file path because it does not reside in the file system: jar:file:/app/youcoach-backend/target/youcoach-backend-1.0-SNAPSHOT.jar!/BOOT-INF/classes!/secured-users.json
    
    Spring Boot cannot read secured-users.json when started as `java -jar youcoach-backend.jar`
    
    This causes problems for Heroku

--- youcoach-backend/src/main/java/com/switchfully/youcoach/security/authentication/user/SecuredUserConfig.java
+++ youcoach-backend/src/main/java/com/switchfully/youcoach/security/authentication/user/SecuredUserConfig.java
@@ -4,20 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Configuration
 public class SecuredUserConfig {
 
+    private final ResourceLoader resourceLoader;
     private PasswordEncoder passwordEncoder;
 
-    public SecuredUserConfig(PasswordEncoder passwordEncoder) {
+    public SecuredUserConfig(PasswordEncoder passwordEncoder, ResourceLoader resourceLoader) {
         this.passwordEncoder = passwordEncoder;
+        this.resourceLoader = resourceLoader;
     }
 
     @Bean
@@ -33,7 +36,8 @@ public class SecuredUserConfig {
 
     private List<SecuredUser> readSecuredUsersFile() {
         try {
-            return Lists.newArrayList(new ObjectMapper().readValue(new ClassPathResource("secured-users.json").getFile(), SecuredUser[].class));
+            URL securedUsers = resourceLoader.getResource("classpath:secured-users.json").getURL();
+            return Lists.newArrayList(new ObjectMapper().readValue(securedUsers, SecuredUser[].class));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
