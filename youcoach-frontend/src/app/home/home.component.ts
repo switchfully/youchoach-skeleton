3249713edbb63b5edb2937d296cbdf4ea9959244
vercruyssetim <tim.vercruysse@cegeka.be>
2020-05-14 10:07:46 +0200

    further improvement on initializing materialize

--- youcoach-frontend/src/app/home/home.component.ts
+++ youcoach-frontend/src/app/home/home.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InitMaterializeComponent} from '../init-materialize.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends InitMaterializeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit(): void {
   }
