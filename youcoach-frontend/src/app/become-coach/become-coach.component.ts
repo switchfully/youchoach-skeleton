e13cb179d6bd514ad0981025f7644972a4e930a3
Sven De Potter <sven.depotter@hotmail.com>
2020-05-14 12:04:38 +0200

    background become a coach fixed

--- youcoach-frontend/src/app/become-coach/become-coach.component.ts
+++ youcoach-frontend/src/app/become-coach/become-coach.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {InitMaterializeComponent} from '../init-materialize.component';
 
 @Component({
   selector: 'app-become-coach',
   templateUrl: './become-coach.component.html',
   styleUrls: ['./become-coach.component.css']
 })
-export class BecomeCoachComponent implements OnInit {
+export class BecomeCoachComponent extends InitMaterializeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
 
   ngOnInit(){
