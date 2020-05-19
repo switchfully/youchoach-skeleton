9e5fdc7b9b15425fba462b7c2a80498d032c394e
Sven De Potter <sven.depotter@hotmail.com>
2020-05-19 13:45:57 +0200

    testing production - round 2

---
+++ youcoach-backend/src/main/resources/sql/delete_all_data.sql
@@ -0,0 +1,9 @@
+delete from youcoach.account_verification;
+delete from youcoach.admins;
+delete from youcoach.coaches_topics;
+delete from youcoach.coaching_topic_grades;
+delete from youcoach.coaching_topics;
+delete from youcoach.coaching_topic;
+delete from youcoach.topics;
+delete from youcoach.coaches;
+delete from youcoach.members;
