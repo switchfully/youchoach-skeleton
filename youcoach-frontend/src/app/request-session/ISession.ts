37a77eceeadd43167d94e381ee006be965d15f76
Akagami1030 <omar.lamrabet@vdab.be>
2020-05-25 15:00:12 +0200

    form request session okay

---
+++ youcoach-frontend/src/app/request-session/ISession.ts
@@ -0,0 +1,8 @@
+export interface ISession {
+  idCoach: number;
+  subject: string;
+  date: string;
+  time: string;
+  location: string;
+  remarks: string;
+}
--- youcoach-frontend/src/app/request-session/request-session.component.ts
+++ youcoach-frontend/src/app/request-session/request-session.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CoachService} from '../coach-profile/coach.service';
 import {ActivatedRoute} from '@angular/router';
+import {ISession} from './ISession';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-request-session',
@@ -8,9 +10,17 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./request-session.component.css']
 })
 export class RequestSessionComponent implements OnInit {
-
+  session: ISession;
+  sessionForm = this.fb.group({
+    idCoach : [''],
+    subject : [''],
+    date : [''],
+    time : [''],
+    location: [''],
+    remarks : [''],
+  });
   idToGet: number;
-  constructor(private coachService: CoachService, private route: ActivatedRoute) {
+  constructor(private coachService: CoachService, private route: ActivatedRoute, private fb: FormBuilder) {
     this.idToGet = +this.route.snapshot.paramMap.get('id');
   }
 
@@ -18,7 +28,16 @@ export class RequestSessionComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  sendRequest() {
 
+  onSubmit() {
+    this.session = {
+      idCoach: this.idToGet,
+      subject: this.sessionForm.get('subject').value,
+      date: this.sessionForm.get('date').value,
+      time: this.sessionForm.get('time').value,
+      location: this.sessionForm.get('location').value,
+      remarks: this.sessionForm.get('remarks').value
+    };
+    alert(this.session.idCoach);
   }
 }
