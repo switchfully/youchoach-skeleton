02bfe441de9aa93247eca79c192192bcaeee1833
nielsh <Niemand@01>
2020-06-05 10:21:49 +0200

    Fixes #22

--- youcoach-frontend/src/app/change-topics/change-topics.component.ts
+++ youcoach-frontend/src/app/change-topics/change-topics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-change-topics',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-topics.component.css']
 })
 export class ChangeTopicsComponent implements OnInit {
+  private mailto: string;
 
-  constructor() { }
+  constructor(public translateService: TranslateService) { }
 
   ngOnInit(): void {
+  }
+
+  mail() {
+    this.mailto = 'mailto:' + environment.adminEmail;
+
+    this.translateService.get('request-profile-change.email-subject').subscribe((subject: string) => {
+      this.mailto += '?subject=' + subject;
+    });
+
+    this.translateService.get('request-profile-change.email-body').subscribe((body: string) => {
+      this.mailto += '&body=' + body;
+    });
+    location.href = this.mailto;
   }
 
 }
