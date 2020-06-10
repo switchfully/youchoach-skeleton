9257092297d237b5c47125c8ddaf8f3b0374cb9e
vercruyssetim <tim.vercruysse@cegeka.be>
2020-06-10 15:47:57 +0200

    added flyyway support

--- youcoach-backend/src/main/resources/db/migration/V1__InitialDatabase.sql
+++ youcoach-backend/src/main/resources/db/migration/V1__InitialDatabase.sql
@@ -2,17 +2,17 @@ create table account_verification
 (
     created_on        timestamp    not null,
     verification_code varchar(255) not null,
-    user_id           number       not null,
+    user_id           integer      not null,
     primary key (user_id)
 );
 create table admins
 (
-    id number not null,
+    id integer not null,
     primary key (id)
 );
 create table coaches
 (
-    id           number not null,
+    id           integer not null,
     availability varchar(255),
     introduction varchar(255),
     xp           integer,
@@ -20,23 +20,23 @@ create table coaches
 );
 create table coaches_topics
 (
-    coach_id number not null,
-    topics_id     number not null
+    coach_id  integer not null,
+    topics_id integer not null
 );
 create table coaching_topic
 (
-    id             number not null,
+    id             integer not null,
     topic_topic_id integer,
     primary key (id)
 );
 create table coaching_topic_grades
 (
-    coaching_topic_id number not null,
+    coaching_topic_id integer not null,
     grades            integer
 );
 create table profile
 (
-    id              number       not null,
+    id              integer      not null,
     account_enabled boolean      not null,
     email           varchar(255) not null,
     first_name      varchar(255),
