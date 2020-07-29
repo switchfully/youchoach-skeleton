2334f11df9c7174bf0d76ac4918e223c57f8f985
vercruyssetim <tim.vercruysse@cegeka.be>
2020-07-29 15:52:34 +0200

    trying out different file types

--- youcoach-backend/src/main/resources/db/migration/V11__Storing_Images.sql
+++ youcoach-backend/src/main/resources/db/migration/V11__Storing_Images.sql
@@ -2,6 +2,6 @@ create table DATABASE_FILE
 (
     file_name varchar(255) not null,
     original_file_name varchar(255) not null,
-    file_content bytea,
+    file_content oid,
     primary key (file_name)
 )
