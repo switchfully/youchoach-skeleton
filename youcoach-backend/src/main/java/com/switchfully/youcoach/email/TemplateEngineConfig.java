eb35af7b5a1966318d6a9939bc58c96cb4d27f49
vercruyssetim <tim.vercruysse@cegeka.be>
2021-06-01 15:47:32 +0200

    Some dependencies changes

--- youcoach-backend/src/main/java/com/switchfully/youcoach/email/TemplateEngineConfig.java
+++ youcoach-backend/src/main/java/com/switchfully/youcoach/email/TemplateEngineConfig.java
@@ -1,13 +1,9 @@
 package com.switchfully.youcoach.email;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.EnvironmentAware;
+import com.google.common.io.Resources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.env.Environment;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
--- youcoach-frontend/src/app/app.module.ts
+++ youcoach-frontend/src/app/app.module.ts
@@ -2,16 +2,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {_MatMenuDirectivesModule, MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
 import {RedirectOnErrorInterceptor} from './redirect-on-error.interceptor';
-import {CustomFormsModule} from 'ngx-custom-validators';
 import {SessionModule} from "./session/session.module";
 import {SecurityModule} from "./security/security.module";
 import {FooterComponent} from "./template/footer/footer.component";
@@ -25,9 +18,9 @@ import {EnHomeComponent} from './template/home/en-home/en-home.component';
 import {FrHomeComponent} from './template/home/fr-home/fr-home.component';
 import {AdminModule} from "./admin/admin.module";
 import {AdminComponent} from './template/admin/admin.component';
-import { FaqComponent } from './template/faq/faq.component';
-import { EnFaqComponent } from './template/faq/en-faq/en-faq.component';
-import { FrFaqComponent } from './template/faq/fr-faq/fr-faq.component';
+import {FaqComponent} from './template/faq/faq.component';
+import {EnFaqComponent} from './template/faq/en-faq/en-faq.component';
+import {FrFaqComponent} from './template/faq/fr-faq/fr-faq.component';
 
 @NgModule({
   declarations: [
